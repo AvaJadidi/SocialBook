63ce6baf266ea733f2da81e1ecd416867a2d0ac0
Ava Jadidi <a.jadidi@ymail.com>
2023-06-15 18:09:11 +0430

    comlete main page and add dark mood

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function settingMenuToggle(){
     settingMenu.classList.toggle("setting-menu-hide");
 
 }
+
+// dark mood
+
+let darkBtn =document.getElementById("dark-btn");
+
+
+darkBtn.onclick= function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+    if (localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme","dark");
+    }
+    else{
+        localStorage.setItem("theme","light");
+
+    }
+}
+// save dark mode or light storage
+if (localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+
+}
+else{
+    localStorage.setItem("theme","light");
+}
